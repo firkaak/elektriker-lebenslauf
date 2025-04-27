8338b283bce1b47d24328a8e120247a4e84d87c8
firkaak <millok.tamas3@gmail.com>
2025-04-27 09:14:38 +0200

    desktop menu und desktopview

--- meinscript.js
+++ meinscript.js
@@ -8,3 +8,21 @@ function toggleMenu(x) {
     dropdown.classList.toggle("show");
 
 } 
+
+// nach Oben button -----
+let mybutton = document.getElementById("oBtn");
+
+window.onscroll = function(){scrollFunktion()};
+
+function scrollFunktion(){
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function obenFunktion() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
